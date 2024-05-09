8247f7fcafaf22738c19be77456962cac2c964ea
qishilong <663253062@qq.com>
2024-05-09 21:45:09 +0800

    fix: 修复logo引入问题

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import AdminController from "@/services/admin";
 import { message } from "antd";
 import type { RequestConfig } from "umi";
 import { history } from "umi";
-import img from "./favicon.png";
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
@@ -48,7 +47,7 @@ export async function getInitialState() {
 
 export const layout = () => {
   return {
-    logo: img,
+    logo: "./favicon.png",
     menu: {
       locale: false
     },
