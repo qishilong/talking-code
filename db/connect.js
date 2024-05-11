a521e5a34d5d521d96e45caf967e1ea5b6e719d9
qishilong <663253062@qq.com>
2024-05-11 23:58:41 +0800

    chore: 添加代码注释

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,6 @@
 /**
  * 该文件负责连接数据库
  */
-
 const mongoose = require("mongoose");
 
 // 定义链接数据库字符串
@@ -10,11 +9,12 @@ const dbURI = "mongodb://" + process.env.DB_HOST + "/" + process.env.DB_NAME;
 // 连接
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// 监听
+// 连接成功监听
 mongoose.connection.on("connected", function () {
   console.log(`${process.env.DB_NAME} 数据库已经连接...`);
 });
 
+// 断开连接监听
 mongoose.connection.on("disconnected", function () {
   console.log(`${process.env.DB_NAME} 数据库断开链接`);
 });
